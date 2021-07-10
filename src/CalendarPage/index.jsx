1fa13acb57c6b3c1822a1b61bdd34b8d6d4edc11
karta0807913 <karta0807913@gmail.com>
2021-07-10 18:16:04 +0800

    change calendar id

--- src/CalendarPage/index.jsx
+++ src/CalendarPage/index.jsx
@@ -4,7 +4,7 @@ export default class CalendarPage extends React.Component {
   render() {
     return (
       <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTaipei&amp;src=NzlsZm5sZjJwamprcDByZzB1dXIyMnVxdDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%237986CB&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0"
+        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTaipei&amp;src=cHA0ZjYwaGptMGxscmYxcHNscWVvYXZobzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%237986CB&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0"
         height="100%"
         frameBorder="0"
         scrolling="no">
